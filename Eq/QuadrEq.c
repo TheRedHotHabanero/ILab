d321e55715c844b5e6aa25467b8ce63defa2fb73
TheRedHotHabanero <lkrn10001@gmail.com>
2020-10-08 19:36:22 +0300

    zero_is_zero conception changed, tab len decreased

--- Eq/QuadrEq.c
+++ Eq/QuadrEq.c
@@ -7,19 +7,20 @@
 #define TWO_ROOTS  2
 #define INF_ROOTS -1
 
-#define FAULT      1e-6
+#define FAULT      1e-8
 
 //================================================================================
 // Greeting function!
-// @note Returns nothing, it`s just for smile
+// @note Returns nothing, it's just for smile
 //---------------------------------------------------------------
 void greetingsq()
 {
-    printf(  "Hi! This is QuadrEq v 2.0\n"
-                    "(c) TheRedHotHabanero\n"
-                    "Solve your quadratic equations!\n" );
+  printf(  "Hi! This is QuadrEq v 2.0\n"
+                  "(c) TheRedHotHabanero\n"
+                  "Solve your quadratic equations!\n\n" );
 }
 //================================================================================
+
 
 //================================================================================
 // Here is input function. The first and the last place
@@ -29,25 +30,31 @@ void greetingsq()
 //---------------------------------------------------------------
 void inputsq(double* pa, double* pb, double* pc)
 {
-    printf("Write equation coefficients in oder a, b, c: ");
-    scanf("%lf%lf%lf", pa, pb, pc);
-    printf("\n");
+  assert(pa && pb && pc);
+
+  printf("Write equation coefficients in oder a, b, c: ");
+  scanf("%lf%lf%lf", pa, pb, pc);
+  printf("\n");
 }
 //================================================================================
 
 
 //================================================================================
 // Zero_is_zero function. It checks the faults of a close to zero numbers.
-// Zero_is_zer returns 0 if the number is too close to zero
-// and return 1 in the opposite situation.
+// Zero_is_zero returns true if the number is too close to zero
+// and returns false in the opposite situation.
 // @param [in] n  - a number that should be transfered to function and checked.
 //---------------------------------------------------------------
 int zero_is_zero(double n)
 {
-    if ( fabs(n) < FAULT )
-        return 0;
-    return 1;
+  return fabs(n) < FAULT;
 }
+
+/*int is_eq(double num1, double num2, double eps = 0)
+{
+  return fabs(num1 - num2) <= eps;
+}*/
+
 //================================================================================
 
 
@@ -61,154 +68,175 @@ int zero_is_zero(double n)
 //---------------------------------------------------------------
 void output(int solutions, double ans1, double ans2)
 {
-    printf("------------------------------------------------------\n");
-    switch (solutions)
-    {
-        case ONE_ROOT:  printf("-- Number of solutions of the equation: %d\n", solutions);
-                        printf("-- x = %lf ", ans1);
-                        break;
+  printf("------------------------------------------------------\n");
+  switch (solutions)
+  {
+    case ONE_ROOT:  printf("| Number of solutions of the equation: %d\n", solutions);
+                    printf("|  x = %lf ", ans1);
+                    break;
 
-        case NO_ROOTS:  printf("-- Equation has no solutions");
-                        break;
+    case NO_ROOTS:  printf("| Equation has no solutions");
+                    break;
 
-        case TWO_ROOTS: printf("-- Number of solutions of the equation: %d\n", solutions);
-                        printf("-- x1 = %lf, x2 = %lf", ans1, ans2);
-                        break;
+    case TWO_ROOTS: printf("| Number of solutions of the equation: %d\n", solutions);
+                    printf("| x1 = %lf, x2 = %lf", ans1, ans2);
+                    break;
 
-        case INF_ROOTS: printf ("-- The number of solutions to the equation is infinite\n");
-                        break;
+    case INF_ROOTS: printf ("| The number of solutions to the equation is infinite\n");
+                    break;
 
-        default:        printf("-- Error: solutions, QuadrEq");
-                        break;
+    default:        printf("| Error: solutions, QuadrEq");
+                    break;
 
-    }
-    printf("\n------------------------------------------------------");
+  }
+  printf("\n------------------------------------------------------");
 }
 //================================================================================
+
 
 //================================================================================
 // Linear equation solution function.
+// @param [in]   a      a - coefficient
+// @param [in]   b      b - coefficient
+// @param [out]  pans1    - solution
+// @note Returns number (!) of solutions, not solutions.
+//---------------------------------------------------------------
 int LineEq(double a, double b, double* pans1)
 {
-    assert(isfinite(a) && isfinite(b));
+  assert(isfinite(a) && isfinite(b));
+  assert(pans1);
 
-    int solutions;
+  int solutions;
 
-    if (zero_is_zero(a) * a != 0)
-        {
-            *pans1 = -b / a;
-            solutions = ONE_ROOT;
-        }
-    else if (zero_is_zero(b) * b != 0)
-            solutions = NO_ROOTS;
-    else
-            solutions = INF_ROOTS;
+  if (!zero_is_zero(a))
+    {
+      *pans1 = -b / a;
+      solutions = ONE_ROOT;
+    }
+  else if (!zero_is_zero(b))
+      solutions = NO_ROOTS;
+  else
+      solutions = INF_ROOTS;
 
-    return (solutions);
+  return (solutions);
 }
+//================================================================================
 
-/*Quadratic equation solution function*/
+
+//================================================================================
+//Quadratic equation solution function
+// @param [in]  a     a - coefficient
+// @param [in]  b     b - coefficient
+// @param [in]  c     c - coefficient
+// @param [out] pans1   - the first (or the one) solution
+// @param [out] pans2   - the second solution
+// @note Like LineEq, returns number (!) of solutions, not solutions.
+//---------------------------------------------------------------
 int QuadrEq(double a, double b, double c, double* pans1, double* pans2)
 {
-    assert(isfinite(a) && isfinite(b) && isfinite(c));
+  assert(isfinite(a) && isfinite(b) && isfinite(c));
+  assert(pans1 && pans2);
 
-    int solutions;
+  int solutions;
 
-    if (zero_is_zero(a) * a == 0)
-        {
-            solutions = LineEq(b, c, pans1);
-            return solutions;
-        }
+  if (zero_is_zero(a))
+    {
+      solutions = LineEq(b, c, pans1);
+      return solutions;
+    }
 
-    double d = b*b - 4*a*c;             //discriminant without fault
-    double D = zero_is_zero(d) * d;     //working discriminant
-    double sqroot = sqrt(D);
+  double D = b*b - 4*a*c;
 
-    if (D > 0)
-        {
-            *pans1 = (-b + sqroot) / (2 * a);
-            *pans2 = (-b - sqroot) / (2 * a);
-            solutions = TWO_ROOTS;
-        }
-    else if (D == 0)
-        {
-            *pans1 = -b / (2 * a);
-            solutions = ONE_ROOT;
-        }
-    else if (D < 0)
-            solutions = NO_ROOTS;
+  if (zero_is_zero(D))
+    {
+      *pans1 = -b / (2 * a);
+      solutions = ONE_ROOT;
+    }
+  else if (D > 0)
+    {
+      double sqroot = sqrt(fabs(D));
+      *pans1 = (-b + sqroot) / (2 * a);
+      *pans2 = (-b - sqroot) / (2 * a);
+      solutions = TWO_ROOTS;
+    }
+  else if (D < 0)
+      solutions = NO_ROOTS;
 
-    return (solutions);
+  return (solutions);
 }
+//================================================================================
 
-/*Checking the program*/
+
+//================================================================================
+// Program checking function
+// Here are some tests with answers. Change something and check how it works!
+// @note Function is not called in main. If you need, uncomment it in main.
+//---------------------------------------------------------------
 void QuadrEqTests()
 {
+  printf("\n\n\nTime to check the program!\n\n");
 
+  int results, lenmas = 6;
+  double root1 = NAN, root2 = NAN;
 
-    printf("\n\n\nTime to check the program!\n\n");
+  //--TEST----------------------1---2---3---4---5---6---
+  double masA[]           =   { 1,  1,  0,  0,  0,  1};
+  double masB[]           =   {-4, -2,  0,  1,  0, -2};
+  double masC[]           =   { 4,  4,  0,  2,  7, -3};
+  double masResNumSol[]   =   { 1,  0, -1,  1,  0,  2};
+  double masResRoots[]    =   { 2,  0,  0, -2,  0,  2};
 
-    int results, lenmas = 6;
-    double root1 = NAN, root2 = NAN;
+  for (int k = 0; k <= lenmas -1; k++)
+  {
+    results = QuadrEq(masA[k], masB[k], masC[k], &root1, &root2);
 
+    if (results != masResNumSol[k])
+      {
+        printf("Test %d BAD, wrong solutions amount. \n", k+1);
+        continue;
+      }
 
-    //--TEST----------------------1---2---3---4---5---6---
-    double masA[]           =   { 1,  1,  0,  0,  0,  1};
-    double masB[]           =   {-4, -2,  0,  1,  0, -2};
-    double masC[]           =   { 4,  4,  0,  2,  7, -3};
-    double masResNumSol[]   =   { 1,  0, -1,  1,  0,  2};
-    double masResRoots[]    =   { 2,  0,  0, -2,  0,  2};
+    switch (results)
+      {
+        case NO_ROOTS:      printf("Test %d Passed \n", (k+1));
+                            break;
 
-    for (int k = 0; k <= lenmas -1; k++)
-    {
-        results = QuadrEq(masA[k], masB[k], masC[k], &root1, &root2);
+        case INF_ROOTS:     printf("Test %d Passed \n", (k+1));
+                            break;
 
-        if (results != masResNumSol[k])
-            {
-                printf("Test %d BAD, wrong solutions amount. \n", k+1);
-                continue;
-            }
+        case ONE_ROOT:      if (root1 * 1.0 == masResRoots[k])
+                                    printf("Test %d Passed \n", (k + 1));
+                            else
+                                    printf("Test %d BAD, wrong root\n", k+1);
+                            break;
 
-        switch (results)
-            {
-                case NO_ROOTS:      printf("Test %d Passed \n", (k+1));
-                                    break;
+        case TWO_ROOTS:     if ((root1 + root2) * 1.0 == masResRoots[k])
+                                    printf("Test %d Passed \n", (k + 1));
+                            else
+                                    printf("Test %d BAD, wrong roots. \n", k+1);
+                            break;
 
-                case INF_ROOTS:     printf("Test %d Passed \n", (k+1));
-                                    break;
-
-                case ONE_ROOT:      if (root1 * 1.0 == masResRoots[k])
-                                            printf("Test %d Passed \n", (k + 1));
-                                    else
-                                            printf("Test %d BAD, wrong root\n", k+1);
-                                    break;
-
-                case TWO_ROOTS:     if ((root1 + root2) * 1.0 == masResRoots[k])
-                                            printf("Test %d Passed \n", (k + 1));
-                                    else
-                                            printf("Test %d BAD, wrong roots. \n", k+1);
-                                    break;
-
-                default:            printf ("Error: QuadrEqTest");
-            }
-    }
+        default:            printf ("Error: QuadrEqTest");
+      }
+  }
 }
+//================================================================================
+
 
 int main()
 {
-    /*Introducing variables*/
-    double a = NAN, b = NAN, c = NAN;
-    double ans1 = NAN, ans2 = NAN;
-    int solnum;
+  //Introducing variables
+  double a = NAN, b = NAN, c = NAN;
+  double ans1 = NAN, ans2 = NAN;
+  int solnum;
 
+  //Functions call
+  greetingsq();
+  inputsq(&a, &b, &c);
+  solnum = QuadrEq(a, b, c, &ans1, &ans2);
+  output(solnum, ans1, ans2);
 
-    /*Functions call*/
-    greetingsq();
-    inputsq(&a, &b, &c);
-    solnum = QuadrEq(a, b, c, &ans1, &ans2);
-    output(solnum, ans1, ans2);
-    //QuadrEqTests();
+  //QuadrEqTests();
 
-    return (0);
-
+  return (0);
 }
